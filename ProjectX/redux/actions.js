e0df371868ab61ae1d00f57026f87f561a92729f
Zain Noor <zainnoor516@gmail.com>
2020-05-06 19:40:16 -0400

    Added action

--- ProjectX/redux/actions.js
+++ ProjectX/redux/actions.js
@@ -1,8 +1,18 @@
-import { SET_USER_ID } from './actionTypes';
+import { 
+  SET_USER_ID,
+  GET_USER_PROJECTS,
+} from './actionTypes';
 
 export const setUserId = (userId) => {
   return {
     type: SET_USER_ID,
     payload: { userId },
+  }
+}
+
+export const getUserProjects = (projects) => {
+  return {
+    type: GET_USER_PROJECTS,
+    payload: { projects },
   }
 }
